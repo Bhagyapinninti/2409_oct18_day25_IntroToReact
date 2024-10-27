5714416d572733480a63c7b8a649c2ad88d69b74
Bhagyapinninti <bagiteja322@gmail.com>
2024-10-27 12:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,133 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Classical Dances of India</h1>
+      <p>
+        India has many classical dance styles, each with its own unique style
+        and regional variations.
+      </p>
+      <ul>
+        <li>Bharatanatyam (Tamil Nadu)</li>
+        <li>Kathak (North India, primarily Uttar Pradesh)</li>
+        <li>Kathakali (Kerala)</li>
+        <li>Kuchipudi (Andhra Pradesh)</li>
+      </ul>
+      <p>
+        Indian classical dancing started around 200 BCE in India, as a joyful
+        and celebratory activity, often in devotion to Hindu deities. Many of
+        the performances are choreographed to retell stories of the gods and
+        other historical accounts. All styles of Indian classical dance are
+        vibrant, expressive, and spiritual. Dance performances usually take
+        place at festivals, universities, various cultural events, and more. The
+        dancers who perform these styles are usually professionals who have
+        devoted years of study and practice in their respective style of Indian
+        classical dance. In performances, the dancers move to the beat of the
+        song or music that is playing; in some styles, such as Kathak, bells are
+        worn around the ankles at times for added rhythmic effect when the feet
+        are stomped. The dancer takes the role of the character that they are
+        portraying in the performance, the composition being specific, and
+        become emotionally connected with the story and the audience.
+      </p>
+
+<div className="dance">
+<h2>Bharathanatyam</h2>
+
+      <img src="./images/bharathanatyam.jpg" alt=""></img>
+      <p>
+        Bharatanatyam is an Indian classical dance form that originated in Tamil
+        Nadu, India. It is a classical dance form recognized by the Sangeet
+        Natak Akademi, and expresses South Indian religious themes and spiritual
+        ideas of Hinduism and Jainism.
+      </p>
+      <p>
+        Bharatanatyam contains different types of bani. Bani, or "tradition", is
+        a term used to describe the dance technique and style specific to a guru
+        or school, often named for the village of the guru. Bharatanatyam style
+        is noted for its fixed upper torso, bent legs, and flexed knees
+        (Aramandi) combined with footwork, and a vocabulary of sign language
+        based on gestures of hands, eyes, and face muscles.The dance is
+        accompanied by music and a singer, and typically the dancer's guru is
+        present as the nattuvanar or director-conductor of the performance and
+        art. The performance repertoire of Bharatanatyam, like other classical
+        dances, includes nrita (pure dance), nritya (Conveys a meaning to the
+        audience through hand gestures) and natya (Consists of the elements of
+        drama). A program of bharatanatyam usually lasts two hours without
+        interruption and includes a specific list of procedures, all performed
+        by one dancer, who does not leave the stage or change costume. The
+        accompanying orchestra—composed of drums, drone, and singer—occupies the
+        back of the stage, led by the guru, or the teacher, of the dancer.
+      </p>
+</div>
+      <div className="dance">
+      <h2>Kathak</h2>
+      <img src="./images/kathak.jpg" alt=""></img>
+      <p>
+        Kathak is one of the nine major forms of Indian classical dance. Its
+        origin is attributed to the traveling bards in ancient northern India
+        known as Kathakar ("storyteller"), who communicated stories from the
+        Hindu epics through dance, songs and music. Its name derives from the
+        Sanskrit word katha which means "story", and kathakar which means "the
+        one who tells a story", or "to do with stories". Kathak dancers tell
+        various stories utilizing hand movements and extensive footwork, their
+        body movements and flexibility as well as their facial expressions.
+        Kathak often has a strong beat and can be danced in many taals. While
+        proto-Kathak elements can be seen long before, Kathak evolved during the
+        Bhakti movement, particularly by incorporating the childhood and stories
+        of the Hindu god Krishna, as well as independently in the courts of
+        north Indian kingdoms. During the period of Mughal rule, the emperors
+        were patrons of Kathak dance and actively promoted it in their royal
+        courts. Kathak performances include Urdu ghazal and commonly used
+        instruments brought during the Mughal period.As a result, it is the only
+        Indian classical dance form to feature Persian elements.
+      </p>
+      </div>
+      <div className="dance">
+      <h2>Kathakali</h2>
+      <img src="./images/kathakali.jpg" alt=""></img>
+      <p>
+        Kathakali is a traditional form of classical Indian dance, and one of
+        the most complex forms of Indian theatre. It is a play of verses . These
+        Verses called Kathakali literature or Attakatha . Mostly played in the
+        courts of Kings and Temple festivals . Hence it known as suvarna art
+        forms . This performance uses the navarasas of Bharathan, the Guru of
+        Indian theatre . Makeup and costumes are very unique and huge . Normally
+        it represent Kerala Art . It is native to the Malayalam-language
+        southwestern region of Kerala and is almost entirely practiced by
+        Malayali people.
+      </p>
+      </div>
+      <div className="dance">
+      <h2>Kuchipudi</h2>
+      <img src="./images/kuchipudi.jpg" alt=""></img>
+      <p>
+        Kuchipudi is one of the eight major Indian classical dances. It
+        originates from a village named Kuchipudi in the Indian state of Andhra
+        Pradesh. Kuchipudi is a dance-drama performance, with its roots in the
+        ancient Hindu Sanskrit text of Natya Shastra.It developed as a religious
+        art linked to traveling bards, temples and spiritual beliefs, like all
+        major classical dances of India. The traditional Kuchipudi was performed
+        by all males troupe. A dancer in a male role would be in Agnivastra,
+        also known as Bagalbandi, wear a dhoti (a single pleated piece of cloth
+        hanging down from the waist). A dancer in a female role would
+        wear a Sari with light makeup.The Kuchipudi performance usually
+        begins with an invocation. Then, each costumed actor is introduced,
+        their role stated, and they then perform a short preliminary dance set
+        to music (dharavu). Next, the performance presents pure dance
+        (nritta). This is followed with by the expressive part of the
+        performance (nritya), where rhythmic hand gestures help convey the
+        story. Vocal and instrumental Carnatic music in the Telugu
+        language accompanies the performance. The typical musical
+        instruments in Kuchipudi are mridangam, cymbals, veena, flute and the
+        tambura. The popularity of Kuchipudi has grown within India and it
+        is performed worldwide.
+      </p>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
